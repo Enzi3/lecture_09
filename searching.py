bf2c9494eba1d0180a4eaa93f5bb7a86aa75a764
Jiri Skladany <jiri.skladany@gmail.com>
2022-04-11 08:56:40 +0200

    commit

--- searching.py
+++ searching.py
@@ -16,7 +16,7 @@ def read_data(file_name, field):
     file_path = os.path.join(cwd_path, file_name)
     with open(file_path, mode="r") as json_file:
         data = json.load(json_file)
-        if field not in set(data.keys())
+        if field not in set(data.keys()):
             return None
     return data[field]
 
